d2607aca7cdf9d08376f041c5d9f0d5562d6703b
Taehwan Seo <jjffkkgg@gmail.com>
2020-08-25 16:48:15 +0900

    add q network comment

--- dqn_demo/inverted_pendulum.py
+++ dqn_demo/inverted_pendulum.py
@@ -48,3 +48,17 @@ class DQN(nn.Module):
     def __init__(self, h, w, outputs):
         super(DQN, self).__init__()                                 # class inheritance from nn.module
         self.conv1 = nn.Conv2d(3,16,kernel_size = 5, stride = 2)    # https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html 
+        self.bn = nn.BatchNorm2d(16)                                # https://pytorch.org/docs/stable/generated/torch.nn.BatchNorm2d.html
+        self.conv2 = nn.Conv2d(16, 32, kernel_size = 5, stride = 2) # 2d 블럭들에 대한 convolution 집합정보 2
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32,32,kernel_size = 5, stride = 2)
+        self.bn3 = nn.BatchNorm2d(32) 
+
+        # conv2d layer output --(affect)--> Linear input connection | compute input image(conv2d) size
+        def conv2d_size_out(size, kernel_size = 5, stride = 2):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))        # width sizeout
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))        # height sizeout
+        linear_input_size = convw * convh * 32                              # w * h * batch size
+        self.head = nn.Linear(linear_input_size, outputs)
